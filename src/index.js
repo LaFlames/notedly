278c5b427386634cdac34759c217e17420839ab8
Vitali <vetalichegga@gmail.com>
2022-10-03 23:31:35 +0300

    feat: add local mongodb database, note schema and resolvers

--- src/index.js
+++ src/index.js
@@ -1,23 +1,55 @@
-// index.js
-// This is the main entry point of our application
 import express from 'express';
 import { gql, ApolloServer } from 'apollo-server-express';
+import dataBase from './db.js';
+import dotenv from 'dotenv';
+import * as models from './models/index.js';
+
+dotenv.config();
 
 const port = process.env.PORT || 4000;
+const DB_HOST = process.env.DB_HOST;
 
 const typeDefs = gql`
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+
   type Query {
-    hello: String
+    hello: String!
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+
+  type Mutation {
+    addNote(content: String!): Note!
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => 'Hello wrld!'
+    notes: async () => {
+      return await models.Note.find();
+    },
+    note: async (_, args) => {
+      return await models.Note.findById(args.id);
+    }
+  },
+
+  Mutation: {
+    addNote: async (_, args) => {
+      return await models.Note.create({
+        content: args.content,
+        author: 'Vitalik'
+      });
+    }
   }
 };
 
 const app = express();
+
+dataBase.connect(DB_HOST);
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
---
+++ src/models/index.js
@@ -0,0 +1,3 @@
+import { default as Note } from './note.js';
+
+export { Note };
